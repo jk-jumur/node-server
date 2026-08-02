c7bed7d3ffd84222a67a6c54947654ef79091628
jk-jumur <developer.jumur@gmail.com>
2026-08-03 02:14:08 +0600

    create a node js project

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+
+    if(req.url === '/'){
+    res.writeHead(200, { 'content-type': 'text/plain'})
+    res.end('Hello from node server');
+
+}
+
+    
+    else if(req.url === '/about'){
+        res.writeHead(200, { 'content-type': 'text/plain'})
+        res.end('this about page of node server');
+    }
+});
+
+server.listen(5000, () => {
+    console.log('Server is running on port 5000');
+}); 
